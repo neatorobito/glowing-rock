6be707623b9f054ad7bb650730d65baa9c63fbc8
Mark Raymond Jr <markrjr@live.com>
2020-11-08 00:13:55 -0800

    Basic TCP client support

--- poc_sock.cr
+++ poc_sock.cr
@@ -1,13 +1,41 @@
 require "socket"
+require "crystal/system/windows"
 
-# https://stackoverflow.com/questions/24131843/what-is-makeword-used-for
-# version = ((2) << 8 | (2))
+version = Crystal::System.make_word(2,2)
 
-# result = LibC.WSAStartup(version, out wsaData)
+result = LibC.WSAStartup(version, out wsaData)
 
-# if(result != 0)
-#   puts "WSAStartup failed: #{result}"
-# else
-#   puts wsaData.wVersion
-#   puts wsaData.wHighVersion
+# {% if flag?(:win32) %}
+#   raise Socket::Error.new("This type of socket is not supported on Windows.")
+# {% end %}
+
+if(result != 0)
+  puts "WSAStartup failed: #{result}"
+else
+  puts "WinSock Version: #{wsaData.wVersion}"
+end
+
+client = Socket.new(Socket::Family::INET, Socket::Type::STREAM, Socket::Protocol::TCP, false)
+client.connect("localhost", 53540)
+client << "Hello from Windows!\n"
+response = client.gets
+client.close
+
+# client = TCPSocket.new("localhost", 53540)
+# client << "Hello again!\n"
+# response = client.gets
+# client.close
+
+# require "socket"
+
+# def handle_client(client)
+#   message = client.gets
+#   client.puts message
 # end
+
+# server = TCPServer.new("localhost", 1234)
+# while client = server.accept?
+#   spawn handle_client(client)
+# end
+
+LibC.WSACleanup()
